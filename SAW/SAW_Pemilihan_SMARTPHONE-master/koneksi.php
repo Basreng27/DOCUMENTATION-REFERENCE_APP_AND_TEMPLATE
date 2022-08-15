61e1ff538b11319d64e73568da753f9ee4fef99a
Rayandra Wandi Marselana <mwandirayandra@gmail.com>
2022-08-15 10:39:09 +0700

    Menambahkan 2 metode SAW

---
+++ SAW/SAW_Pemilihan_SMARTPHONE-master/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "spk_hp";
+$connect = mysqli_connect($host, $user, $pass, $db) or die("Gagal Connect ke Database");
+// $db = mysqli_select_db($db) or die("Database Tidak Ada");
